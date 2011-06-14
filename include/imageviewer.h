7c0662479f68b2631d518703cc3f310dba861a6b
rickyviking@gmail.com <rickyviking@gmail.com@742c7a3f-2b40-113f-15ff-d8e96d1db33d>
2011-06-14 21:46:35 +0000

    little reformatting, update scene rectangle when a new image is loaded.

--- include/imageviewer.h
+++ include/imageviewer.h
@@ -1,42 +1,3 @@
-/****************************************************************************
-**
-** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** You may use this file under the terms of the BSD license as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of Nokia Corporation and its Subsidiary(-ies) nor
-**     the names of its contributors may be used to endorse or promote
-**     products derived from this software without specific prior written
-**     permission.
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-** $QT_END_LICENSE$
-**
-****************************************************************************/
 
 #ifndef IMAGEVIEWER_H
 #define IMAGEVIEWER_H
@@ -69,24 +30,24 @@ public:
       }
 
 
-   void paint(QPainter *painter, const QStyleOptionGraphicsItem *,
-      QWidget *)
-   {
-      QColor color(Qt::blue);
-      if (_alive)
-         color = Qt::yellow;
+      void paint(QPainter *painter, const QStyleOptionGraphicsItem *,
+         QWidget *)
+      {
+         QColor color(Qt::blue);
+         if (_alive)
+            color = Qt::yellow;
 
-      color.setAlphaF(0.7);
-      painter->fillRect(rect(), color);
-   }
+         color.setAlphaF(0.7);
+         painter->fillRect(rect(), color);
+      }
 
-   void mousePressEvent(QGraphicsSceneMouseEvent *event)
-   {
-      _alive = !_alive;
-      update();
-   }
+      void mousePressEvent(QGraphicsSceneMouseEvent *event)
+      {
+         _alive = !_alive;
+         update();
+      }
 
-   bool _alive;
+      bool _alive;
 };
 
 
@@ -95,51 +56,54 @@ public:
 //! [0]
 class ImageViewer : public QMainWindow
 {
-    Q_OBJECT
+   Q_OBJECT
 
 public:
-    ImageViewer();
+   ImageViewer();
 
-private slots:
-    void open();
-    void print();
-    void zoomIn();
-    void zoomOut();
-    void normalSize();
-    void fitToWindow();
-    void about();
+   void wheelEvent(QWheelEvent * event);
+
+   private slots:
+      void open();
+      void print();
+      void zoomIn();
+      void zoomOut();
+      void normalSize();
+      void fitToWindow();
+      void about();
 
 private:
-    void createActions();
-    void createMenus();
-    void updateActions();
-    void scaleImage(double factor);
-    void adjustScrollBar(QScrollBar *scrollBar, double factor);
+   void createActions();
+   void createMenus();
+   void updateActions();
+   void scaleImage(double factor);
+   void adjustScrollBar(QScrollBar *scrollBar, double factor);
 
-    QLabel *imageLabel;
-    QScrollArea *scrollArea;
-    double scaleFactor;
+   QLabel *imageLabel;
+   QScrollArea *scrollArea;
+   double scaleFactor;
 
 #ifndef QT_NO_PRINTER
-    QPrinter printer;
+   QPrinter printer;
 #endif
 
-    QAction *openAct;
-    QAction *printAct;
-    QAction *exitAct;
-    QAction *zoomInAct;
-    QAction *zoomOutAct;
-    QAction *normalSizeAct;
-    QAction *fitToWindowAct;
-    QAction *aboutAct;
-    QAction *aboutQtAct;
+   QAction *openAct;
+   QAction *printAct;
+   QAction *exitAct;
+   QAction *zoomInAct;
+   QAction *zoomOutAct;
+   QAction *normalSizeAct;
+   QAction *fitToWindowAct;
+   QAction *aboutAct;
+   QAction *aboutQtAct;
 
-    QMenu *fileMenu;
-    QMenu *viewMenu;
-    QMenu *helpMenu;
+   QMenu *fileMenu;
+   QMenu *viewMenu;
+   QMenu *helpMenu;
 
 
-    QGraphicsScene* _pScene;
+   QGraphicsScene* _pScene;
+   QGraphicsView* _pGraphicsView;
 };
 //! [0]
 
